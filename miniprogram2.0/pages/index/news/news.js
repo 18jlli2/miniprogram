10159e0e21e54a2227ab702f55d787bea0a21c81
18jlli2 <1621479531@qq.com>
2019-05-10 03:35:43 +0800

    new mini program

---
+++ miniprogram2.0/pages/index/news/news.js
@@ -0,0 +1,114 @@
+// pages/index/news/news.js
+var app=getApp();
+Page({
+  
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title:'',
+    input: '',
+
+  },
+  inputChangeHandle: function (e) {
+    this.setData({ input: e.detail.value })
+  },
+
+  inputtitle: function (e) {
+    
+    this.setData({
+      title: e.detail.value
+    })
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    if (options.taskTitle) {
+      
+      that.setData({
+        input: options.taskContain,
+        title: options.taskTitle
+      })
+    }
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  openAlert: function (e) {
+
+    if (this.data.title) {
+      var index = e.currentTarget.dataset.index
+      let contains = { contain: this.data.input };
+      app.globalData.contains.push(contains);
+      wx.redirectTo({
+        url: '../index?taskContain='+this.data.input+'&taskTitle='+this.data.title,
+      })
+    }
+    else {
+      wx.showModal({
+        content: '请输入标题',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      });
+    }
+    
+
+  }
+
+})
---
+++ miniprogram2.0/pages/logs/logs.js
@@ -0,0 +1,8 @@
+Page({
+  data:{
+    logs: []
+  },
+  onShow: function () {
+ 
+  },
+})
